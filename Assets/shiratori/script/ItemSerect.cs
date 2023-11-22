b7ce3320d704f39f1b6d199a9169a874615b26a9
白鳥謙信 <h1202315001@hiratagakuen.onmicrosoft.com>
2023-11-22 09:47:31 +0900

    ItemSelect.cs完成？

--- Assets/shiratori/script/ItemSerect.cs
+++ Assets/shiratori/script/ItemSerect.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ItemSerect : MonoBehaviour
 {
+
     // ItemSelectUIを格納する変数
     // インスペクターからゲームオブジェクトを設定する
     [SerializeField] GameObject ItemSelectUI;
-    [SerializeField] GameObject[] Item = new GameObject[12];
+    //[SerializeField] GameObject[] Item = new GameObject[12];
+
+    GameObject datainfo;
+    ItemManager itemmanager = new ItemManager();
+
+    public Text[] textname = new Text[3];
+    public Text[] textdisc = new Text[3];
+    //　TextMeshProUGUI   メッシュプろを使う際はこっち
+    private void Start()
+    {
+        datainfo = GameObject.Find("DataInfo");
+        itemmanager = datainfo.GetComponent<ItemManager>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,37 +37,51 @@ public class ItemSerect : MonoBehaviour
     {
         ItemSelectUI.SetActive(true);
 
+        textname[0].text = "asdjaisd";
+
+        for (int i = 0; i < 3; i++)
+        {
+            string Id = itemmanager.GetRandomItem();
+            textname[i].text = itemmanager.GetName(Id);
+            textdisc[i].text = itemmanager.GetDescription(Id);
+        }
+
+        //string Id =itemmanager.GetRandomItem();
+        //string name = itemmanager.GetName(Id);
+        //string discription = itemmanager.GetDescription(Id);
+
+        //itemname = name;
         // ランダムに3つのアイテムをアクティブにする
-        ActivateRandomItems(3);
+        //ActivateRandomItems(3);
     }
 
-    void ActivateRandomItems(int count)
-    {
-        // すべてのアイテムを非アクティブにする
-        foreach (var item in Item)
-        {
-            item.SetActive(false);
-        }
+    //void ActivateRandomItems(int count)
+    //{
+    //    // すべてのアイテムを非アクティブにする
+    //    foreach (var item in Item)
+    //    {
+    //        item.SetActive(false);
+    //    }
 
-        List<int> selectedIndices = new List<int>();
+    //    List<int> selectedIndices = new List<int>();
 
-        //ランダムなアイテムを選択
-        while (selectedIndices.Count < count)
-        {
-            int randomIndex = Random.Range(0, Item.Length);
-            if (!selectedIndices.Contains(randomIndex))
-            {
-                selectedIndices.Add(randomIndex);
-            }
-        }
+    //    //ランダムなアイテムを選択
+    //    while (selectedIndices.Count < count)
+    //    {
+    //        int randomIndex = Random.Range(0, Item.Length);
+    //        if (!selectedIndices.Contains(randomIndex))
+    //        {
+    //            selectedIndices.Add(randomIndex);
+    //        }
+    //    }
 
-        // 選択されたアイテムをアクティブにする
-        foreach (int index in selectedIndices)
-        {
-            if (index >= 0 && index < Item.Length)
-            {
-                Item[index].SetActive(true);
-            }
-        }
-    }
+    //    // 選択されたアイテムをアクティブにする
+    //    foreach (int index in selectedIndices)
+    //    {
+    //        if (index >= 0 && index < Item.Length)
+    //        {
+    //            Item[index].SetActive(true);
+    //        }
+    //    }
+    //}
 }
